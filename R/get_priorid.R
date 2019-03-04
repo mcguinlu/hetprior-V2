6caaef97607e18d67b17a58a0db8b6365ceaa875
Luke McGuinness <41202585+mcguinlu@users.noreply.github.com>
2019-03-04 15:03:58 +0000

    Updated version with links to external packages

--- R/get_priorid.R
+++ R/get_priorid.R
@@ -7,75 +7,75 @@
 
 get_priorid <- function() {
   ui <- miniUI::miniPage(
-    gadgetTitleBar("Prior ID Selection Tool"),
-    miniContentPanel(
+    miniUI::gadgetTitleBar("Prior ID Selection Tool"),
+    miniUI::miniContentPanel(
       scrollable = FALSE,
-      p("Please complete the boxes below, and select submit to view the relevant prior."),
-      p("Once you are happy with your selection, click 'Done' to export the Prior ID to R for use with", em("hetprior")),
-      p("Note: choices are updated based on your input into the previous boxes."),
+      shiny::p("Please complete the boxes below, and select submit to view the relevant prior."),
+      shiny::p("Once you are happy with your selection, click 'Done' to export the Prior ID to R for use with", shiny::em("hetprior")),
+      shiny::p("Note: choices are updated based on your input into the previous boxes."),
 
-      fillRow(
-      fillCol(
-      h3("Conditions:"),
-      selectizeInput("hetstat", label = "Heterogeneity statistic:",
+      shiny::fillRow(
+      shiny::fillCol(
+      shiny::h3("Conditions:"),
+      shiny::selectizeInput("hetstat", label = "Heterogeneity statistic:",
                        choices = c(unique(as.character(hetdata$Heterogeneity.statistic))),
                        options = list(placeholder = 'Choose',
                                       onInitialize = I('function() { this.setValue(""); }'))),
-      uiOutput("datatypeui"),
-      uiOutput("effectmeasureui"),
-      uiOutput("distformui"),
-      uiOutput("interventiontypeui"),
-      uiOutput("natureoutcomeui"),
-      uiOutput("medicalareaui"),
-      uiOutput("samplesizeui"),
-      uiOutput("actiondoui"),
-      actionButton("clear","Clear")),
+      shiny::uiOutput("datatypeui"),
+      shiny::uiOutput("effectmeasureui"),
+      shiny::uiOutput("distformui"),
+      shiny::uiOutput("interventiontypeui"),
+      shiny::uiOutput("natureoutcomeui"),
+      shiny::uiOutput("medicalareaui"),
+      shiny::uiOutput("samplesizeui"),
+      shiny::uiOutput("actiondoui"),
+      shiny::actionButton("clear","Clear")),
 
-      fillCol(
-      h3("Results:"),
-      uiOutput("prioridui"),
-      uiOutput("meanui"),
-      uiOutput("sdui"),
+      shiny::fillCol(
+      shiny::h3("Results:"),
+      shiny::uiOutput("prioridui"),
+      shiny::uiOutput("meanui"),
+      shiny::uiOutput("sdui"),
       height = "40%")
       )
     )
   )
 
-  server <- function(input, output, session) {
+server <- function(input, output, session) {
     v <- reactiveValues(data = NULL, buttonclick = 0)
-    observeEvent(input$hetstat,{
+    shiny::observeEvent(input$hetstat,{
       v$buttonclick <- 0
-      updateSelectInput(session,'datatype',
+      shiny::updateSelectInput(session,'datatype',
                         choices = c("", unique(as.character(hetdata$Data.Type[hetdata$Heterogeneity.statistic==input$hetstat]))))
     })
 
-    observeEvent(input$datatype,{
+    shiny::observeEvent(input$datatype,{
       v$buttonclick <- 0
-      updateSelectInput(session,'effectmeasure',
+      shiny::updateSelectInput(session,'effectmeasure',
                         choices = c("", unique(as.character(hetdata$Effect.Measure[hetdata$Heterogeneity.statistic==input$hetstat &
                                                                                      hetdata$Data.Type==input$datatype]))))
     })
 
-    observeEvent(input$effectmeasure,{
+    shiny::observeEvent(input$effectmeasure,{
       v$buttonclick <- 0
-      updateSelectInput(session,'distributionform',
+      shiny::updateSelectInput(session,'distributionform',
                         choices = c("", unique(as.character(hetdata$Distribution.form[hetdata$Heterogeneity.statistic==input$hetstat &
                                                                                         hetdata$Data.Type==input$datatype &
                                                                                         hetdata$Effect.Measure==input$effectmeasure]))))
     })
 
-    observeEvent(input$distributionform,{
+    shiny::observeEvent(input$distributionform,{
       v$buttonclick <- 0
-      updateSelectInput(session,'interventiontype',
+      shiny::updateSelectInput(session,'interventiontype',
                         choices = c("", unique(as.character(hetdata$Type.of.Intervention[hetdata$Heterogeneity.statistic==input$hetstat &
                                                                                            hetdata$Data.Type==input$datatype &
                                                                                            hetdata$Effect.Measure==input$effectmeasure &
                                                                                            hetdata$Distribution.form==input$distributionform]))))
     })
 
-    observeEvent(input$interventiontype,{
+    shiny::observeEvent(input$interventiontype,{
       v$buttonclick <- 0
-      updateSelectInput(session,'natureoutcome',
+      shiny::updateSelectInput(session,'natureoutcome',
                         choices = c("", unique(as.character(hetdata$Nature.of.Outcome[hetdata$Heterogeneity.statistic==input$hetstat &
                                                                                         hetdata$Data.Type==input$datatype &
                                                                                         hetdata$Effect.Measure==input$effectmeasure &
@@ -83,9 +83,9 @@ get_priorid <- function() {
                                                                                         hetdata$Type.of.Intervention==input$interventiontype]))))
     })
 
-    observeEvent(input$natureoutcome,{
+    shiny::observeEvent(input$natureoutcome,{
       v$buttonclick <- 0
-      updateSelectInput(session,'medicalarea',
+      shiny::updateSelectInput(session,'medicalarea',
                         choices = c("", unique(as.character(hetdata$Medical.area[hetdata$Heterogeneity.statistic==input$hetstat &
                                                                                    hetdata$Data.Type==input$datatype &
                                                                                    hetdata$Effect.Measure==input$effectmeasure &
@@ -95,9 +95,9 @@ get_priorid <- function() {
                                                                                  ]))))
     })
 
-    observeEvent(input$medicalarea,{
+    shiny::observeEvent(input$medicalarea,{
       v$buttonclick <- 0
-      updateSelectInput(session,'samplesize',
+      shiny::updateSelectInput(session,'samplesize',
                         choices = c("", unique(as.character(hetdata$Average.Sample.Size[hetdata$Heterogeneity.statistic==input$hetstat &
                                                                                           hetdata$Data.Type==input$datatype &
                                                                                           hetdata$Effect.Measure==input$effectmeasure &
@@ -109,57 +109,57 @@ get_priorid <- function() {
     })
 
     output$datatypeui <-
-      renderUI({
+      shiny::renderUI({
         if (is.null(input$hetstat)){}else{
           if (input$hetstat!=""){
-            selectizeInput("datatype", label = "Data type:",
+            shiny::selectizeInput("datatype", label = "Data type:",
                            choices = NULL,
                            options = list(placeholder = 'Choose',
                                           onInitialize = I('function() { this.setValue(""); }')))
           }}})
 
     output$effectmeasureui <-
-      renderUI({
+      shiny::renderUI({
         if (is.null(input$datatype)){}else{
           if (input$hetstat!="" & input$datatype!=""){
-            selectizeInput("effectmeasure", label = "Effect measure:",
+            shiny::selectizeInput("effectmeasure", label = "Effect measure:",
                            choices = NULL,
                            options = list(placeholder = 'Choose',
                                           onInitialize = I('function() { this.setValue(""); }')))
           }}})
 
     output$distformui <-
-      renderUI({
+      shiny::renderUI({
         if (is.null(input$effectmeasure)){}else{
           if (input$hetstat!="" & input$datatype!="" & input$effectmeasure!=""){
-            selectizeInput("distributionform", label = "Distribution form:",
+            shiny::selectizeInput("distributionform", label = "Distribution form:",
                            choices = NULL,
                            options = list(placeholder = 'Choose',
                                           onInitialize = I('function() { this.setValue(""); }')))
           }}})
 
     output$interventiontypeui <-
-      renderUI({
+      shiny::renderUI({
         if (is.null(input$distributionform)){}else{
           if (input$hetstat!="" & input$datatype!="" & input$effectmeasure!="" & input$distributionform!=""){
-            selectizeInput("interventiontype", label = "Type of intervention:",
+            shiny::selectizeInput("interventiontype", label = "Type of intervention:",
                            choices = NULL,
                            options = list(placeholder = 'Choose',
                                           onInitialize = I('function() { this.setValue(""); }')))
           }}})
 
     output$natureoutcomeui <-
-      renderUI({
+      shiny::renderUI({
         if (is.null(input$interventiontype)){}else{
           if (input$hetstat!="" & input$datatype!="" & input$effectmeasure!="" & input$distributionform!="" & input$interventiontype!=""){
-            selectizeInput("natureoutcome", label = "Nature of Outcome:",
+            shiny::selectizeInput("natureoutcome", label = "Nature of Outcome:",
                            choices = NULL,
                            options = list(placeholder = 'Choose',
                                           onInitialize = I('function() { this.setValue(""); }')))
           }}})
 
     output$medicalareaui <-
-      renderUI({
+      shiny::renderUI({
         if (is.null(input$natureoutcome)){}else{
           if (input$hetstat!="" &
               input$datatype!="" &
@@ -168,14 +168,14 @@ get_priorid <- function() {
               input$interventiontype!="" &
               input$natureoutcome!=""
               ){
-            selectizeInput("medicalarea", label = "Medical area:",
+            shiny::selectizeInput("medicalarea", label = "Medical area:",
                            choices = NULL,
                            options = list(placeholder = 'Choose',
                                           onInitialize = I('function() { this.setValue(""); }')))
           }}})
 
     output$samplesizeui <-
-      renderUI({
+      shiny::renderUI({
         if (is.null(input$natureoutcome)){}else{
           if (input$hetstat!="" &
               input$datatype!="" &
@@ -184,14 +184,14 @@ get_priorid <- function() {
               input$interventiontype!="" &
               input$natureoutcome!=""
               ){
-            selectizeInput("samplesize", label = "Average sample size:",
+            shiny::selectizeInput("samplesize", label = "Average sample size:",
                            choices = NULL,
                            options = list(placeholder = 'Choose',
                                           onInitialize = I('function() { this.setValue(""); }')))
           }}})
 
     output$actiondoui <-
-      renderUI({
+      shiny::renderUI({
         if (is.null(input$medicalarea) |is.null(input$samplesize)){}else{
           if (input$hetstat!="" &
               input$datatype!="" &
@@ -202,11 +202,11 @@ get_priorid <- function() {
               input$medicalarea!=""&
               input$samplesize!=""
           ){
-            actionButton("do", "Submit")
+            shiny::actionButton("do", "Submit")
             }}})
 
 
-observeEvent(input$do, {
+shiny::observeEvent(input$do, {
       v$data <- subset(hetdata,
                        Heterogeneity.statistic == input$hetstat &
                          Data.Type == input$datatype &
@@ -228,7 +228,7 @@ observeEvent(input$do, {
       v$buttonclick <- 1
       })
 
-observeEvent(input$clear, {
+shiny::observeEvent(input$clear, {
 
       v$buttonclick <- 0
       v$data <- NULL
@@ -243,33 +243,33 @@ observeEvent(input$clear, {
       v$reference <- NULL
     })
 
-output$priorid <- renderText(v$priorid)
-output$mean <- renderText(v$mean)
-output$sd <- renderText(v$sd)
-output$median <- renderText(as.character(v$median))
-output$reference <- renderText(as.character(v$reference))
-output$lowquant <- renderText(as.character(v$lowquant))
-output$highquant <- renderText(as.character(v$highquant))
-output$notes1 <- renderText(as.character(v$notes1))
+output$priorid <- shiny::renderText(v$priorid)
+output$mean <- shiny::renderText(v$mean)
+output$sd <- shiny::renderText(v$sd)
+output$median <- shiny::renderText(as.character(v$median))
+output$reference <- shiny::renderText(as.character(v$reference))
+output$lowquant <- shiny::renderText(as.character(v$lowquant))
+output$highquant <- shiny::renderText(as.character(v$highquant))
+output$notes1 <- shiny::renderText(as.character(v$notes1))
 
 output$prioridui <-
-  renderUI({p(em("Prior ID:"), textOutput("priorid"))})
+  shiny::renderUI({shiny::p(shiny::em("Prior ID:"), shiny::textOutput("priorid"))})
 
 output$meanui <-
-  renderUI({
-    p(em("Mean/Shape:"), textOutput("mean"))})
+  shiny::renderUI({
+    shiny::p(shiny::em("Mean/Shape:"), shiny::textOutput("mean"))})
 
 output$sdui <-
-  renderUI({p(em("Standard deviation/scale:"), textOutput("sd"))})
+  shiny::renderUI({shiny::p(shiny::em("Standard deviation/scale:"), shiny::textOutput("sd"))})
 
 
-    observeEvent(input$done, {
-      stopApp(v$priorid)
+    shiny::observeEvent(input$done, {
+      shiny::stopApp(v$priorid)
     })
-    observeEvent(input$cancel, {
-      stopApp(stop("No prior id.", call. = FALSE))
+    shiny::observeEvent(input$cancel, {
+      shiny::stopApp(stop("No prior id.", call. = FALSE))
     })
   }
 
-  runGadget(ui, server, viewer = dialogViewer("Prior ID", width = 800, height = 900))
+  shiny::runGadget(ui, server, viewer = shiny::dialogViewer("Prior ID", width = 800, height = 900))
 }
